a9a77d510856739ff0d59c3f3d8e8704dbcbb5a1
lhw8420 <1980370947@qq.com>
2018-01-25 17:40:26 +0800

    4.5处理less  自动添加兼容

--- webpack-test002/src/app.js
+++ webpack-test002/src/app.js
@@ -1,8 +1,8 @@
 import layer from './components/layer/layer';
+import './css/common.css';
 
 const App  = function () {
     const NUM = 1;
-    alert(NUM);
     console.log(layer);
 }
 
--- webpack-test002/webpack.config.js
+++ webpack-test002/webpack.config.js
@@ -2,6 +2,7 @@ var htmlWebpackPlugin = require('html-webpack-plugin');
 var path = require('path');
 
 module.exports = {
+    context: __dirname,
     entry: './src/app.js',
     output: {
         path: __dirname + '/dist',
@@ -17,6 +18,72 @@ module.exports = {
                 query: {
                     "presets": ["env"]
                 }
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    "style-loader",
+                    {
+                      loader: "css-loader",
+                        options: {
+                            importLoaders: 1
+                        }
+                    },
+                    {
+                        loader:'postcss-loader',
+                        options:{
+                            plugins:function(){
+                                return [
+                                    require('postcss-import')(),        //一定要写在require("autoprefixer")前面，否则require("autoprefixer")无效
+                                    require("autoprefixer")({browsers:['last 5 versions']})
+                                ]
+                            }
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.less$/,
+                use: [
+                    "style-loader",
+                    {
+                        loader: "css-loader",
+                        options: {
+                            importLoaders: 1
+                        }
+                    },
+                    {
+                        loader:'postcss-loader',
+                        options:{
+                            plugins:function(){
+                                return [
+                                    require('postcss-import')(),        //一定要写在require("autoprefixer")前面，否则require("autoprefixer")无效
+                                    require("autoprefixer")({browsers:['last 5 versions']})
+                                ]
+                            }
+                        }
+                    },
+                    "less-loader"
+                ]
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    "style-loader",
+                    "css-loader",
+                    {
+                        loader:'postcss-loader',
+                        options:{
+                            plugins:function(){
+                                return [
+                                    require('postcss-import')(),        //一定要写在require("autoprefixer")前面，否则require("autoprefixer")无效
+                                    require("autoprefixer")({browsers:['last 5 versions']})
+                                ]
+                            }
+                        }
+                    },
+                    "sass-loader"
+                ]
             }
         ]
     },
